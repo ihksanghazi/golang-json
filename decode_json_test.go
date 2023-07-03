439d2bdefa09ef619d33acbcae0c0861cbb6836d
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-07-03 22:18:37 +0700

    decode json

---
+++ decode_json_test.go
@@ -0,0 +1,25 @@
+package golang_json
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestDecodeJSON(t *testing.T){
+	jsonString := `{"Firstname":"Nur","Middlename":"Sandy","Lastname":"Ihksan","Age":21,"Married":false}`
+	jsonBytes:= []byte(jsonString)
+
+	customer:= &Customer{}
+	err:=json.Unmarshal(jsonBytes,customer)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(customer)
+	fmt.Println(customer.Firstname)
+	fmt.Println(customer.Middlename)
+	fmt.Println(customer.Lastname)
+	fmt.Println(customer.Age)
+	fmt.Println(customer.Married)
+}
